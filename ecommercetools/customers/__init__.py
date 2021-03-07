e6ee4132624f3dabc1a2b5579a5769e6bfc9360b
Matt Clarke <matt@flyandlure.org>
2021-03-07 13:14:46 +0000

    Added get_latency()

--- ecommercetools/customers/__init__.py
+++ ecommercetools/customers/__init__.py
@@ -4,6 +4,8 @@ from ecommercetools.customers.customers import get_abc_segments
 from ecommercetools.customers.customers import get_cohorts
 from ecommercetools.customers.customers import get_cohort_matrix
 from ecommercetools.customers.customers import get_retention
+from ecommercetools.customers.customers import get_latency
+
 
 
 
